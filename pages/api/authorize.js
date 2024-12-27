47f243e085c90f5a524d053525649474c38f1609
Choi Un Don <undon.choi@finenex.net>
2024-12-27 16:16:13 +0900

    버그 수정

--- pages/api/authorize.js
+++ pages/api/authorize.js
@@ -1,6 +1,10 @@
 import {mobileInfo, MY_URL, newOrderNo, request} from "@/utils/common";
 
 export default async function handler(req, res) {
+    if (!req.query.AMOUNT || !req.query.ORDER || !req.query.RRN || !req.query.INT_REF) {
+        throw new Error('Missing parameters');
+    }
+
     const isReal = req.query.MODE === 'R';
     const data = {
         AMOUNT: req.query.AMOUNT ?? '355.00',
@@ -15,12 +19,12 @@ export default async function handler(req, res) {
         TERMINAL: isReal ? '90033670' : '88888881',
         MERCH_GMT: '+5',
         TRTYPE: '0',
-        BACKREF: 'javascript:history.back()',
+        BACKREF: `${MY_URL}/api/authorize${isReal ? '?MODE=R' : ''}`,
         JUST: 'ru',
         MK_TOKEN: 'MERCH',
         NOTIFY_URL: `${MY_URL}/api/authorized`,
-        CLIENT_IP: req.socket.remoteAddress ?? '0.0.0.0',
-        M_INFO: mobileInfo(1920, 1080, '7', '7475558888'),
+        CLIENT_IP: '0.0.0.0',
+        M_INFO: mobileInfo(req.query.WIDTH ?? 1920, req.query.HEIGHT ?? 1080, req.query.CC ?? '', req.query.PHONE ?? ''),
     };
     res.status(200).end(await request(data, isReal));
 }
--- pages/api/purchase.js
+++ pages/api/purchase.js
@@ -6,19 +6,19 @@ export default async function handler(req, res) {
         AMOUNT: req.query.AMOUNT ?? '355.00',
         CURRENCY: '398',
         ORDER: req.query.ORDER ?? newOrderNo(),
-        MERCH_RN_ID: isReal ? 'AF5POST90033670' : '',
+        MERCH_RN_ID: '',
         DESC: req.query.DESC ?? 'Test',
         MERCHANT: 'FINENEX',
         MERCHANT_NAME: 'TOO FINENEX',
         TERMINAL: isReal ? '90033670' : '88888881',
         MERCH_GMT: '+5',
         TRTYPE: '1',
-        BACKREF: 'javascript:history.back()',
+        BACKREF: `${MY_URL}/api/purchase${isReal ? '?MODE=R' : ''}`,
         JUST: 'ru',
         MK_TOKEN: 'MERCH',
         NOTIFY_URL: `${MY_URL}/api/purchased`,
-        CLIENT_IP: req.socket.remoteAddress ?? '0.0.0.0',
-        M_INFO: mobileInfo(1920, 1080, '7', '7475558888'),
+        CLIENT_IP: '0.0.0.0',
+        M_INFO: mobileInfo(req.query.WIDTH ?? 1920, req.query.HEIGHT ?? 1080, req.query.CC ?? '', req.query.PHONE ?? ''),
     };
     res.status(200).end(await request(data, isReal));
 }
